e899b0b3ac5e995978d38c73396ad1b411972059
nguyenkimkhoi2709 <nguyenkimkhoi2709@gmail.com>
2022-07-06 03:05:11 -0700

    add

---
+++ src/api/apiConfig.js
@@ -0,0 +1,11 @@
+
+
+const apiConfig = {
+  baseUrl: 'https://api.themoviedb.org/3/',
+  apiKey: '2060b2cf3499bc7980686e2d6e0d1efe',
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
+}
+
+export default apiConfig
+
