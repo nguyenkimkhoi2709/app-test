604bb7406f1011891aa9b6da152ec9adbe55c03a
nguyenkimkhoi2709 <nguyenkimkhoi2709@gmail.com>
2022-07-06 23:11:04 -0700

    add

--- src/components/hero-slide/HeroSlide.jsx
+++ src/components/hero-slide/HeroSlide.jsx
@@ -23,7 +23,7 @@ const HeroSlide = () => {
             const params = {page: 1}
             try {
                 const response = await tmdbApi.getMoviesList(movieType.popular, {params});
-                setMovieItems(response.results.slice(1, 4));
+                setMovieItems(response.results.slice(2, 6));
                 console.log(response);
             } catch {
                 console.log('error');
